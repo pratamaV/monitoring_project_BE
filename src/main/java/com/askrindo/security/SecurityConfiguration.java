6bf8916a4272107eb9d45752e057cd92a7aa64a3
unknown <1309rizki@gmail.com>
2021-03-14 19:21:15 +0700

    [IMPROVE] add access path

--- src/main/java/com/askrindo/security/SecurityConfiguration.java
+++ src/main/java/com/askrindo/security/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/signup");
+                .antMatchers("/signup", "/division", "/divisions");
     }
     @Override
     @Bean
