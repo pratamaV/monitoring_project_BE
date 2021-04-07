15eb8926744b4b6f42c2aa1753cff7736b4de980
tripurwanti <tripurwanti05@gmail.com>
2021-04-07 11:53:05 +0700

    [IMPROVE] cleaning project

--- src/main/java/com/askrindo/repository/SequenceIdReleaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.askrindo.repository;
-
-import com.askrindo.entity.sequence.SequenceIdRelease;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SequenceIdReleaseRepository extends JpaRepository<SequenceIdRelease, String> {
-}
--- src/main/java/com/askrindo/repository/SequenceIdTaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.askrindo.repository;
-
-import com.askrindo.entity.sequence.SequenceIdTask;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SequenceIdTaskRepository extends JpaRepository<SequenceIdTask, String> {
-}
